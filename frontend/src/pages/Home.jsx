b1e71f5bfacc74f46e5466f1cf42d28ec9c6b19d
inforimrepairpro <inforimrepairpro@gmail.com>
2026-09-15 21:19:29 -0700

    Reorder homepage for stronger customer conversion flow

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,34 +1,26 @@
 import React from 'react';
 import Navbar from '../components/Navbar';
 import Hero from '../components/Hero';
-import Stats from '../components/Stats';
 import Features from '../components/Features';
-import BrandsTicker from '../components/BrandsTicker';
 import Services from '../components/Services';
 import Gallery from '../components/Gallery';
-import Pricing from '../components/Pricing';
 import Process from '../components/Process';
-import Testimonials from '../components/Testimonials';
-import FAQ from '../components/FAQ';
 import ServiceAreas from '../components/ServiceAreas';
+import FAQ from '../components/FAQ';
 import CTA from '../components/CTA';
 import Footer from '../components/Footer';
 
 const Home = () => (
-  <div className="min-h-screen bg-[#0a0a0b]">
+  <div className="min-h-screen bg-[#081526]">
     <Navbar />
     <Hero />
-    <Stats />
     <Features />
-    <BrandsTicker />
     <Services />
     <Gallery />
-    <Pricing />
     <Process />
-    <Testimonials />
-    <FAQ />
-    <ServiceAreas />
     <CTA />
+    <ServiceAreas />
+    <FAQ />
     <Footer />
   </div>
 );
